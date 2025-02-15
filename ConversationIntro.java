a415ed09757ae9563edd9942641aa73ecb5a3ac7
yunxiann <yding41@smith.edu>
2025-02-15 10:19:45 -0500

    Finshed first attempt of ConservationItro

--- ConversationIntro.java
+++ ConversationIntro.java
@@ -9,8 +9,7 @@ public class ConversationIntro {
   
   /** Returns an integer one larger than its input */
   public int plusOne(int n) {
-    // n = n+1;
-    return n;
+    return n + 1;
   }
 
   /*
@@ -21,7 +20,7 @@ public class ConversationIntro {
   
   /** Returns an integer that is double its input */
   public int timesTwo(int n) {
-    return 0;
+    return n * 2;
   }
 
   /*
@@ -30,7 +29,7 @@ public class ConversationIntro {
   
   /** Returns the square of its input */
   public int squared(int n) {
-    return 0;
+    return n * (1/2);
   }
 
   /*
@@ -39,7 +38,7 @@ public class ConversationIntro {
   
   /** Divides its input by seven, dropping remainders */
   public int intDivideBy7(int n) {
-    return 0;
+    return n / 7;
   }
 
   /*
@@ -48,7 +47,7 @@ public class ConversationIntro {
   
   /** Divides its input by 7 */
   public double doubleDivideBy7(int n) {
-    return 0;
+    return n / 7.0;
   }
 
   /*
@@ -63,11 +62,11 @@ public class ConversationIntro {
   /** Returns true if its input is prime */
   public static boolean isPrime(int n) {
     boolean isPrime = (n > 1); // one, zero, and negatives not prime
-    /*for (int i = 2; i <= Math.sqrt(n); i++) {
+    for (int i = 2; i <= Math.sqrt(n); i++) {
         if (n%i == 0) {
             isPrime = false;
         }
-    }*/
+    }
     return isPrime;
   }
 
@@ -77,6 +76,9 @@ public class ConversationIntro {
   
   /** Returns true if its input is even */
   public boolean isEven(int n) {
+    if (n % 2 == 0) {
+      return true;
+    }
     return false;
   }
 
@@ -92,8 +94,7 @@ public class ConversationIntro {
   
   /** Returns true if its input is a comma */
   public static boolean isComma(String s) {
-    //return s.equals(",");
-    return (s == ",");
+    return s.equals(",");
   }
 
   /*
@@ -102,7 +103,7 @@ public class ConversationIntro {
   
   /** Returns true if its input is one of:  .,?! */
   public boolean isPunctuation(String s) {
-    return false;
+    return s.equals(".") || s.equals(",") || s.equals("?") || s.equals("!");
   }
 
   /*
@@ -111,6 +112,12 @@ public class ConversationIntro {
   
   /** Returns its input, unless it is "I" or "you", which switch */
   public String mirrorWord(String s) {
+    if (s.equals("I")) {
+      return "you";
+    }
+    if (s.equals("you")) {
+      return "I";
+    }
     return s;
   }
 
@@ -125,9 +132,9 @@ public class ConversationIntro {
   /** Returns an array of whole numbers up to n */
   public static int[] wholeNumbers(int n) {
     int[] arr = new int[n]; // allocate space for the array
-    //for (int i = 0; i < n; i++) {
-    //  arr[i] = i+1;
-    //}
+    for (int i = 0; i < n; i++) {
+      arr[i] = i+1;
+    }
     return arr;
   }
 
@@ -139,6 +146,9 @@ public class ConversationIntro {
   /** Returns an array of squares from 1 to n*n */
   public static int[] squares(int n) {
     int[] arr = new int[n]; // allocate space for the array
+    for (int i = 1; i < n * n; i++) {
+      arr[i] = (i+1) * (i+1);
+    }
     return arr;
   }
 }
